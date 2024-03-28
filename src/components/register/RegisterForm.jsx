5b7849a1e6e64655a172c888ff31a8a6ecc4f276
Lukas Naujunas <l.naujunas@gmail.com>
2024-03-28 21:10:11 +0200

    Changes on form

--- src/components/register/RegisterForm.jsx
+++ src/components/register/RegisterForm.jsx
@@ -2,23 +2,24 @@ import style from '../register/RegisterForm.module.css'
 
 export function Registerform() {
     return (
-         <form className={style.regForm}>
-            <div className={style.userInput}>
-                <label htmlFor="">Username:</label>
-                <input type="text" placeholder="Eg. John"></input>
+         <form className={style.form}>
+            <div className={style.formRow}>
+                <label className={style.label} htmlFor="">Username:</label>
+                <input className={style.input} type="text" placeholder="Eg. John"></input>
             </div>
-            <div className={style.userInput}>
-                <label htmlFor="">Email:</label>
-                <input type="text" placeholder="Eg. John@cena.com"></input>
+            <div className={style.formRow}>
+                <label className={style.label} htmlFor="">Email:</label>
+                <input className={style.input} type="text" placeholder="Eg. John@cena.com"></input>
             </div>
-            <div className={style.userInput}>
-                <label htmlFor="">Password:</label>
-                <input type="text" placeholder="Eg. cantseemenow"></input>
+            <div className={style.formRow}>
+                <label className={style.label} htmlFor="">Password:</label>
+                <input className={style.input} type="text" placeholder="Eg. cantseemenow"></input>
             </div>            
-            <div className={style.userInput}>
-                <label htmlFor="">Repeat password:</label>
-                <input type="text" placeholder="Eg. cantseemenow"></input>
+            <div className={style.formRow}>
+                <label className={style.label} htmlFor="">Repeat password:</label>
+                <input className={style.input} type="text" placeholder="Eg. cantseemenow"></input>
             </div>
+            <button className={style.button}>Register</button>
         </form>
     );
 }
